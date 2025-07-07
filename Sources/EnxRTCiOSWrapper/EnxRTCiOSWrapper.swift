8ba117a7ebb73398e5f93dec518277ae82fe3c86
smartflomeet <atul.titare@tatatel.co.in>
2025-07-07 13:19:27 +0530

    Fixed dependency version

---
+++ Sources/EnxRTCiOSWrapper/EnxRTCiOSWrapper.swift
@@ -0,0 +1,11 @@
+// EnxRTCiOSWrapper.swift
+import Foundation
+
+// Re-export EnablexWebRTC so users can access it automatically
+@_exported import EnablexWebRTC
+
+// Re-export EnxRTCiOS (your main binary framework)
+@_exported import EnxRTCiOS
+
+// Re-export public APIs if needed
+// You can add wrapper functions or re-export specific types here
